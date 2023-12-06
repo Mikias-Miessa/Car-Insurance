76f250fbe91e8618af9083f17fb53f6985997b36
Dagim Asnake <dagimasnake1@gmail>
2023-12-06 22:35:41 +0300

    added navbar

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,8 @@ import Home from '@/components/home/Home'
 
 const CarInsurance = () => {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-9xl text-center">
+    <div>
         <Home />
-      </h1>
   </div>
   )
 }
