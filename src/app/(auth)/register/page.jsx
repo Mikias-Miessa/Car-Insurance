6065827ba03cbfef682f07b009bab49091ce33cd
Dagim Asnake <dagimasnake1@gmail>
2023-12-10 05:56:16 +0300

    added registeration

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,13 +1,107 @@
-import React from 'react'
+"use client"
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Register = () => {
-  return (
-    <div id='register' className="min-h-screen flex items-center justify-center">
-    <h1 className="text-center">
-      Register
-    </h1>
-  </div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
-export default Register
+  const handleBlur = (field) => {
+    // Clear the specific error when the field is blurred
+    setErrors((prevErrors) => ({ ...prevErrors, [field]: '' }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validationErrors = {};
+    if (!email) {
+      validationErrors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      validationErrors.email = 'Invalid email format';
+    }
+    if (!password) {
+      validationErrors.password = 'Password is required';
+    }
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    // Submit the form
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    // Reset form fields and errors
+    setEmail('');
+    setPassword('');
+    setErrors({});
+  };
+
+  return (
+    <div id='register' className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h2 className="text-2xl font-semibold mb-6 text-center">Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className={`${
+                errors.email ? 'border-red-500' : ''
+              } shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+              id="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onBlur={() => handleBlur('email')}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-xs italic">{errors.email}</p>
+            )}
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className={`${
+                errors.password ? 'border-red-500' : ''
+              } shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+              id="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onBlur={() => handleBlur('password')}
+            />
+            {errors.password && (
+              <p className="text-red-500 text-xs italic">{errors.password}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 focus:outline-none"
+          >
+            Register
+          </button>
+        </form>
+        <Link href="/login" className="block text-blue-500 mt-5 text-base text-center">
+          Already have an account?
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
