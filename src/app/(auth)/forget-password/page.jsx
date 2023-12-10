917ecc59fdcc9693156b7da43cf38f3984be3282
Dagim Asnake <dagimasnake1@gmail>
2023-12-10 06:27:15 +0300

    added forget password

--- src/app/(auth)/forget-password/page.jsx
+++ src/app/(auth)/forget-password/page.jsx
@@ -2,9 +2,8 @@
 import { useState } from 'react';
 import Link from 'next/link';
 
-const Login = () => {
+const forgetPassword = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [errors, setErrors] = useState({});
 
   const handleBlur = (field) => {
@@ -21,9 +20,6 @@ const Login = () => {
     } else if (!/\S+@\S+\.\S+/.test(email)) {
       validationErrors.email = 'Invalid email format';
     }
-    if (!password) {
-      validationErrors.password = 'Password is required';
-    }
 
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
@@ -32,18 +28,16 @@ const Login = () => {
 
     // Submit the form
     console.log('Email:', email);
-    console.log('Password:', password);
 
     // Reset form fields and errors
     setEmail('');
-    setPassword('');
     setErrors({});
   };
 
   return (
-    <div id='login' className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div id='forget' className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-96">
-        <h2 className="text-2xl font-semibold mb-6 text-center">Login</h2>
+        <h2 className="text-2xl font-semibold mb-6 text-center">Forgot Password</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
@@ -67,46 +61,19 @@ const Login = () => {
               <p className="text-red-500 text-xs italic">{errors.email}</p>
             )}
           </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className={`${
-                errors.password ? 'border-red-500' : ''
-              } shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
-              id="password"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              onBlur={() => handleBlur('password')}
-            />
-            {errors.password && (
-              <p className="text-red-500 text-xs italic">{errors.password}</p>
-            )}
-          </div>
           <button
             type="submit"
             className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 focus:outline-none"
           >
-            Login
+            Submit
           </button>
         </form>
-        <div className="mt-2 flex justify-between w-full max-w-md mx-auto">
-        <Link href="/register" className="text-blue-500 text-sm hover:underline">
-          Don't have an account?
+        <Link href="/register" className="block text-blue-500 mt-2 text-base text-center hover:underline">
+          Not registered yet? Register now
         </Link>
-        <Link href="/forget-password" className="text-blue-500 text-sm hover:underline">
-          Forgot password?
-        </Link>
-      </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default forgetPassword;
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 const notfound = () => {
   return (
-    <div class="min-h-screen flex flex-grow items-center justify-center mx-5 md:mx-0">
-        <div class="rounded-lg bg-white p-8 text-center shadow-xl">
-            <h1 class="mb-4 text-4xl font-bold">404</h1>
-            <p class="text-gray-600">Oops! The page you are looking for could not be found.</p>
-            <Link href="/" class="mt-4 inline-block rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"> Go back to Home </Link>
+    <div className="min-h-screen flex flex-grow items-center justify-center mx-5 md:mx-0">
+        <div className="rounded-lg bg-white p-8 text-center shadow-xl">
+            <h1 className="mb-4 text-4xl font-bold">404</h1>
+            <p className="text-gray-600">Oops! The page you are looking for could not be found.</p>
+            <Link href="/" className="mt-4 inline-block rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600"> Go back to Home </Link>
         </div>
     </div>
   )
